e6c08aea528a3f71dbf1987a3193c2ad7a8b0e17
max123 <marinhomax3@gmail.com>
2020-04-08 09:24:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -78,9 +78,9 @@ bot.on('message', message=>{
    
         break;
         case 'cat'
-            let message = await message.channel.send("generating...");
+            let message = message.channel.send("generating...");
 
-            let {body} = await superagent;
+            let {body} = superagent;
             .get('http://aws.random.cat/meow')
             console.log(body.file)
         break;
