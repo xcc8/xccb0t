fd171438e7a1b3aab423263fe2455ef01fae6071
max123 <marinhomax3@gmail.com>
2020-04-13 13:33:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -109,38 +109,7 @@ bot.on('message', async message=>{
             msg.delete();
 
         break;
-/*        case 'puptest':
-
-            var reddit = [
-        "meme",
-        "animemes",
-        "MemesOfAnime",
-        "animememes",
-        "AnimeFunny",
-        "dankmemes",
-        "dankmeme",
-        "wholesomememes",
-        "MemeEconomy",
-        "techsupportanimals",
-        "meirl",
-        "me_irl",
-        "2meirl4meirl",
-        "AdviceAnimals"
-    ]
-
-    var subreddit = reddit[Math.floor(Math.random() * reddit.length)];
-
-    message.channel.startTyping();
-
-    randomPuppy(subreddit).then(async url => {
-            await message.channel.send({
-                files: [{
-                    attachment: url,
-                    name: 'meme.png'
-                }]
-            }).then(() => message.channel.stopTyping());
-    }).catch(err => console.error(err));
-
+        case 'puptest':
 
           randomPuppy()
     .then(url => {
@@ -148,7 +117,7 @@ bot.on('message', async message=>{
     })
     
         break;
-*/
+
 	}
 })
 
